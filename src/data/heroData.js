509e618a6e9e3d03c7512a013c9eeb07fd8795e3
Bea <117057809+Milkmelona@users.noreply.github.com>
2023-10-25 10:41:50 +0800

    update hero data

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -20,6 +20,7 @@ const heroImages = [
         src: hero1,
         compSrc: compressedHero1,
         alt: "a woman showing her left hand wearing multiple accessories",
+        name: "Capella",
         desc:  "Shine with the stars in our celestial-inspired necklace, a celestial touch to your style."
     },
 
@@ -28,6 +29,7 @@ const heroImages = [
         src: hero2,
         compSrc: compressedHero2,
         alt: "a woman's neck wearing multiple accessories",
+        name: "Albireo",
         desc: "Unveil the rare beauty of nature with our pearl collection, a tribute to elegance and femininity."
     },
 
@@ -36,6 +38,7 @@ const heroImages = [
         src: hero3,
         compSrc: compressedHero3,
         alt: "bracelets and earings spread on a silk fabric",
+        name: "Deneb",
         desc: "Embrace the timeless elegance of our lustrous pearl bracelet, a symbol of refined beauty and sophistication."
     },
     {
@@ -43,6 +46,7 @@ const heroImages = [
         src: hero4,
         compSrc: compressedHero4,
         alt: "ring on a hand on the sand",
+        name: "Castor",
         desc: "Channel the spirit of the ocean with our diamond ring, a symbol of serenity and grace."
     },
     {
@@ -50,6 +54,7 @@ const heroImages = [
         src: hero5,
         compSrc: compressedHero5,
         alt: "ring on a hand on the sand",
+        name: "Pollux",
         desc: "Elegance meets luxury with our timeless diamond-studded bangle, a statement piece for all occasions."
     },
     {
@@ -57,6 +62,7 @@ const heroImages = [
         src: hero6,
         compSrc: compressedHero6,
         alt: "woman with multiple bracelets and rings",
+        name: "Alkaid",
         desc: "Elevate your wrist with the allure of our exquisite gold bracelets, the perfect blend of luxury and style."
     },
 ]
