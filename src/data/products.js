0693c77f2217b381a2ba7dd9eedae7f62c6a8b68
Bea <117057809+Milkmelona@users.noreply.github.com>
2023-10-28 19:10:33 +0800

    Update product images and display

--- src/data/products.js
+++ src/data/products.js
@@ -13,77 +13,134 @@ import img12 from "../assets/Products/product-img-12.jpg"
 import img13 from "../assets/Products/product-img-13.jpg"
 import img14 from "../assets/Products/product-img-14.jpg"
 
+import img1Comp from "../assets/Compressed Unsplash Images/product-img-1-min.jpg"
+import img2Comp from "../assets/Compressed Unsplash Images/product-img-2-min.jpg"
+import img3Comp from "../assets/Compressed Unsplash Images/product-img-3-min.jpg"
+import img4Comp from "../assets/Compressed Unsplash Images/product-img-4-min.jpg"
+import img5Comp from "../assets/Compressed Unsplash Images/product-img-5-min.jpg"
+import img6Comp from "../assets/Compressed Unsplash Images/product-img-6-min.jpg"
+import img7Comp from "../assets/Compressed Unsplash Images/product-img-7-min.jpg"
+import img8Comp from "../assets/Compressed Unsplash Images/product-img-8-min.jpg"
+import img9Comp from "../assets/Compressed Unsplash Images/product-img-9-min.jpg"
+import img10Comp from "../assets/Compressed Unsplash Images/product-img-10-min.jpg"
+import img11Comp from "../assets/Compressed Unsplash Images/product-img-11-min.jpg"
+import img12Comp from "../assets/Compressed Unsplash Images/product-img-12-min.jpg"
+import img13Comp from "../assets/Compressed Unsplash Images/product-img-13-min.jpg"
+import img14Comp from "../assets/Compressed Unsplash Images/product-img-14-min.jpg"
+
 
 const products = [
     {
         name: "Pavo",
         src: img1,
-        price: "8500"
+        compSrc: img1Comp,
+        price: "8500",
+        alt: "Gold ring, earrings, and bracelet",
+        id: "product-img-1"
     },
     {
         name: "Monoceros",
         src: img2,
-        price: "12000"
+        compSrc: img2Comp,
+        price: "12000",
+        alt: "Stoned floral earrings",
+        id: "product-img-2"
     },
     {
         name: "Cetus",
         src: img3,
-        price: "17000"
+        compSrc: img3Comp,
+        price: "17000",
+        alt: "Gold chained bracelet",
+        id: "product-img-3"
     },
     {
         name: "Hydrus",
         src: img4,
-        price: "15000"
+        compSrc: img4Comp,
+        price: "15000",
+        alt: "Colorful rings",
+        id: "product-img-4"
     },
     {
         name: "Auriga",
         src: img5,
-        price: "13000"
+        placeHolder: img5Comp,
+        price: "13000",
+        alt: "Gold ring with stoned heart design",
+        id: "product-img-5"
     },
     {
         name: "Norma",
         src: img6,
-        price: "12500"
+        compSrc: img6Comp,
+        price: "12500",
+        alt: "Diamond ring",
+        id: "product-img-6"
     },
     {
         name: "Dubhe",
         src: img7,
-        price: "11500"
+        compSrc: img7Comp,
+        price: "11500",
+        alt: "Gold ring with pinkk sapphire stone",
+        id: "product-img-7"
     },
     {
         name: "Hamal",
         src: img8,
-        price: "13500"
+        compSrc: img8Comp,
+        price: "13500",
+        alt: "Gold ring with diamond stone",
+        id: "product-img-8"
     },
     {
         name: "Alkaid",
         src: img9,
-        price: "12500"
+        compSrc: img9Comp,
+        price: "12500",
+        alt: "Pearl necklace with stoned floral pendant",
+        id: "product-img-9"
     },
     {
         name: "Vega",
         src: img10,
-        price: "14500"
+        price: "14500",
+        compSrc: img10Comp,
+        alt: "Gold earrings with celestial design",
+        id: "product-img-10"
     },
     {
         name: "Mirach",
         src: img11,
-        price: "15000"
+        compSrc: img11Comp,
+        price: "15000",
+        alt: "Pearl earrings",
+        id: "product-img-11"
     },
     {
         name: "Alphecca",
         src: img12,
-        price: "12500"
+        compSrc: img12Comp,
+        price: "12500",
+        alt: "Gold necklace with curved dragon pendant",
+        id: "product-img-12"
     },
     {
         name:"Altair",
         src: img13,
-        price: "14500"
+        compSrc: img13Comp,
+        price: "14500",
+        alt: "Gold earrings",
+        id: "product-img-13"
     },
     {
         name: "Algol",
         src: img14,
-        price: "16500"
+        compSrc: img14Comp,
+        price: "16500",
+        alt: "Studded silver bracelet",
+        id: "product-img-14"
     }
 
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,22 +1,58 @@
 import React from "react";
+import products from "../data/products";
+import bannerImg from "../assets/Products/products-banner-img.jpg"
+import bannerImgComp from "../assets/Compressed Unsplash Images/products-banner-img-min.jpg"
+import ProgressiveImage from "react-progressive-graceful-image";
 
 function Shop(){
     return(
         <div className="shop">
+            <section className="shop__banner">
+            <ProgressiveImage
+            src= {bannerImg}
+            placeholder = {bannerImgComp}>
+                {(src, loading) => (
+                    <img
+                    src = {src}
+                    className = {`banner-item__img ${
+                        loading && 'img--loading'}`
+                    }
+                    loading = "lazy"
+                    />
+                )}
+            </ProgressiveImage>
+            </section>
             <ul className="shop__categories">
                 <li>New Arrival</li>
                 <li>Sale!</li>
+                <li>All Products</li>
                 <li>Earrings</li>
                 <li>Bracelet</li>
                 <li>Necklace</li>
                 <li>Anklet</li>
                 <li>Ring</li>
             </ul>
-            <div className="shop__products">
-                <div className="shop__products-item">
-                    
-                </div>
-            </div>
+            {/* <section className="shop__products">
+                {products.map((product) => (
+                    <div className="shop__products-item"  key={product.id}>
+                        <ProgressiveImage 
+                            src={product.src}
+                            placeholder={product.compSrc}
+                            >
+                            {(src, loading) => (
+                                <img
+                                src = {src}
+                                alt = {product.alt}
+                                className = {`product-item__img ${
+                                    loading && 'img--loading'}`
+                                }
+                                loading = "lazy"
+                                />
+                            )}
+                        </ProgressiveImage>
+                    </div>
+                )) }
+            </section> */}
         </div>
     )
 }
