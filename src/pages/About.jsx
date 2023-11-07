833d42025eb03c2c5dc677fdbc1f9a03ef3fb998
Bea <117057809+Milkmelona@users.noreply.github.com>
2023-11-07 12:29:29 +0800

    Set up styling for about page for mobile and small screens

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import "../styles/about.scss"
+import "../styles/about.scss";
+import img1 from "../assets/Unsplash Images/about-img-1.jpg";
+import img2 from "../assets/Unsplash Images/about-img-2.jpg";
+import img1Comp from "../assets/Compressed-Unsplash-Images/about-img-1-min.jpg"
+import img2Comp from "../assets/Compressed-Unsplash-Images/about-img-2-min.jpg"
+import Imgwrapper from "../components/Imagewrapper";
+import ProgressiveImage from "react-progressive-graceful-image";
+import Button from "../components/Buttons";
 
 function About(){
     return(
@@ -10,15 +17,69 @@ function About(){
                 We believe that every woman is a shining star in her own right, 
                 and our mission is to adorn her with celestial treasures that capture the essence of the cosmos.</p>
         </section>
-        <section className="about__history">
+        <section className="about__info-container">
+        <div className="about__history">
+            <Imgwrapper className="about__img-wrapper">
+            <ProgressiveImage
+                src={img1}
+                placeholder={img1Comp}
+                >
+                    {(src, loading) => (
+                        <img
+                        src = {src}
+                        alt = "Woman in a silk dress wearing necklaces"
+                        className = {`about__img img1 ${
+                            loading && 'img--loading'}`
+                        }
+                        loading = "lazy"
+                        />
+                    )}
+            </ProgressiveImage>
+            </Imgwrapper>
+            <div className="about__text-wrapper">
             <h3>The Birth of Cosmic Charms</h3>
             <p>Our story began with a small group of artisans who shared a common love for celestial wonders. This vision led to the birth of Cosmic Charms, 
             a jewelry store unlike any other, where we aimed to capture the essence of the universe in every piece.</p>
-        </section>
-        <section className="about__advocacy">
+            <Button
+                    text = {"Shop Now"}
+                    color = {"#c4aeac"}
+                    fontSize ={21}
+                    type = {"button"}
+                    className = {"hero-item__wrapper-btn"}
+                    />
+            </div>
+        </div>
+        <div className="about__advocacy">
+            <Imgwrapper className="about__img-wrapper">
+            <ProgressiveImage
+                src={img2}
+                placeholder={img2Comp}
+                >
+                    {(src, loading) => (
+                        <img
+                        src = {src}
+                        alt = "Woman in a silk dress wearing necklaces"
+                        className = {`about__img img1 ${
+                            loading && 'img--loading'}`
+                        }
+                        loading = "lazy"
+                        />
+                    )}
+            </ProgressiveImage>
+            </Imgwrapper>
+            <div className="about__text-wrapper">
             <h3>Empowering Women Through Time</h3>
-            <p>Eempowering women to embrace their own inner light, strength, and individuality. 
+            <p>Empowering women to embrace their own inner light, strength, and individuality. 
             We believe that our jewelry serves as a bridge between the ancient wisdom of the stars and the modern woman's journey.</p>
+            <Button
+                    text = {"Shop Now"}
+                    color = {"#c4aeac"}
+                    fontSize ={21}
+                    type = {"button"}
+                    className = {"hero-item__wrapper-btn"}
+                    />
+            </div>
+        </div>
         </section>
         <section className="about__values">
             <div className="about__values-container">
@@ -38,23 +99,25 @@ function About(){
             </div>
         </section>
         <section className="about__locations">
+            <div className="about__text-wrapper loc">
             <h3>Discover Our Showroom Locations</h3>
             <p>While our celestial jewelry is available online for your convenience, we also believe in the beauty of physical experiences. 
                We have thoughtfully curated showrooms in several enchanting locations where you can explore our celestial creations in person.</p>
+            </div>
             <div className="about__locations-main">
-            <ul className="about__locations-container">
+            <ul>
                 <h3>Stellar Haven, New York City</h3>
                 <li>Address: 123 Celestial Avenue, Suite 101, New York, NY</li>
                 <li>Hours: Monday to Saturday, 10:00 AM - 7:00 PM</li>
                 <li>Phone: (555) 123-4567</li>
             </ul>
-            <ul className="about__locations-container">
+            <ul>
                 <h3>Astral Adornments, Paris</h3>
                 <li>Address: 101 Etoile Boulevard, Paris, France</li>
                 <li>Hours: Monday to Saturday, 10:00 AM - 7:00 PM</li>
                 <li>Phone: +33 1 2345 6789</li>
             </ul>
-            <ul className="about__locations-container">
+            <ul>
                 <h3>Galaxy Gems, London</h3>
                 <li>Address: 789 Orion Road, London, UK</li>
                 <li>Hours: Monday to Friday, 9:30 AM - 6:30 PM; Saturday, 10:00 AM - 4:00 PM</li>
