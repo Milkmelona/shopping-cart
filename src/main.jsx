c663b28f4c276e10eb9f0340466f5e5cc7bf3494
Bea <117057809+Milkmelona@users.noreply.github.com>
2023-11-20 13:12:35 +0800

    Add initial paths to the pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import './index.scss';
+import Homepage from './pages/Homepage'
+import Header from './components/Header'
+import Footer from "../src/components/Footer"
+import HeaderSticky from './components/Header-sticky'
+import Collection from './pages/Collectionpage'
+import Shop from './pages/Shop'
+import Faqs from './pages/faqs'
+import About from './pages/About'
+import ContextProvider from './context/ContextProvider';
+
+function MainRoutes(){
+  const[IsSticky, setIsSticky] = useState(false)
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            if (window.pageYOffset > 0) {
+                setIsSticky(true)
+            }
+            else {
+                setIsSticky(false)
+            }
+        }
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
+  return (
+      <BrowserRouter>
+        <ContextProvider>
+          {IsSticky ? (
+            <HeaderSticky/>
+            ) : (
+            <Header/>)}
+          <Routes>
+              <Route index path="/" element={<Homepage/>}/>
+              <Route path="/collection" element={<Collection/>}/>
+              <Route path="/shop" element={<Shop/>}/>
+              <Route path="/faqs" element={<Faqs/>}/>
+              <Route path="/about" element={<About/>}/>
+          </Routes>
+          <Footer/>
+        </ContextProvider>
+      </BrowserRouter>
+
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <MainRoutes/>
   </React.StrictMode>,
 )
